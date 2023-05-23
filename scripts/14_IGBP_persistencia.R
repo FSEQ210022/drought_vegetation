cfc6201fcef5a30dede2557361f40533cd2cb09b
francisco <frzambra@gmail.com>
2023-05-23 12:39:39 -0400

    hacer analisis landcover y tendencia

---
+++ scripts/14_IGBP_persistencia.R
@@ -0,0 +1,63 @@
+# Analyzing IGBP landcover for Chile
+# by Francisco Zambrano (frzambra@gmail.com)
+# April 2021
+
+dir <- '/mnt/md0/raster_procesada/MODIS/'
+dir.out <- '/mnt/md0/raster_procesada/MODIS_derived/IGBP.MCD12Q1.061/'
+
+library(terra)
+library(rnaturalearth)
+library(fs)
+library(sf)
+
+chl <- ne_countries(country='chile',returnclass = 'sf') |> 
+  st_transform(32719)
+
+files <- dir_ls(paste0(dir,'IGBP.MCD12Q1.061'),regexp = '*.tif$')
+igbp <- rast(files)
+igbp <- crop(igbp,chl)
+
+# calculate the most common IGBP class for the 19 years
+# and the persistance (%)
+
+igbpfun1 <- function(px){
+  if (!all(is.na(px))){
+    lv <- sort(table(px),decreasing = TRUE)[1]
+    perc <- lv/length(px)
+    lclass <- ifelse(px[length(px)] == as.numeric(names(lv)),1,0)
+    class <- as.numeric(names(lv))
+    out <- c(perc*100,class,lclass)
+  } else out <- c(NA,NA,NA)
+}
+
+#due to a conflict with {parallel} this line must be executed 
+parallel:::setDefaultClusterOptions(setup_strategy = "sequential")
+
+system.time(
+  igbpPC <- app(igbp,fun=igbpfun1,cores=80)                 
+)
+writeRaster(igbpPC,paste0(dir.out,'IGBP_perc_class_lastClass.tif'),
+            wopt=list(datatype='INT1U'),overwrite = TRUE)
+
+# selecting the IGBP class that remains for >X% of the years
+
+prop <- 80
+selct <- function(x,prop){
+  out <- NA
+  if(!is.na(x[1])){
+    out <- ifelse(x[1] < prop, NA,x[2])
+  }
+  out
+}
+
+igbpProp <- app(igbpPC,fun=selct,cores = 80,prop=80)                   
+
+# reclassify with {terra}
+
+classes <- matrix(c(1:17,rep(1,5),rep(2,2),rep(3,2),4,5,6,7,6,8,9,10),ncol=2)
+
+igbpRecl <- classify(igbpProp,rcl=classes,include.lowest = TRUE)
+igbpRecl[igbpRecl == 0] <- NA
+
+writeRaster(igbpRecl,paste0(dir.out,'IGBP',prop,'_reclassified.tif'),
+            wopt=list(datatype='INT1U'))
---
+++ scripts/15_IGBP_variacion_tendencia_2001-2021.R
@@ -0,0 +1,80 @@
+# Analyzing landcover variation through central Chile
+# August 2021
+# by frzambra
+
+dir <- '/mnt/md0/raster_procesada/MODIS/'
+
+paleta <- read.csv('data/processed_data/paleta_colores_landcover.csv')
+colors <-  rgb(paleta$R,paleta$G,paleta$B,maxColorValue = 255)
+attr(colors,'names') <- paleta$Name
+
+library(terra)
+library(rnaturalearth)
+library(sf)
+
+chl <- ne_countries(country = 'chile',returnclass = 'sf') |> 
+  st_transform(32719)
+
+igbp <- rast(list.files(paste0(dir,'IGBP.MCD12Q1.061'),pattern='*.tif$',full.names=TRUE))
+igbp <- crop(igbp,chl)
+
+classes <- matrix(c(1:17,rep(1,5),rep(2,2),rep(3,2),4,5,6,7,6,8,9,10),ncol=2)
+
+igbpRecl <- classify(igbp,rcl=classes,include.lowest = TRUE)
+igbpRecl[igbpRecl == 0] <- NA
+
+zones <- read_sf('data/processed_data/spatial/macrozonas_chile.gpkg') |> 
+  st_transform(32719)
+data <- terra::extract(igbpRecl,zones)
+names(data) <- c('zone',2001:2021)
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+data |> 
+  pivot_longer(-zone,values_to = 'LC_type',names_to = 'year') |> 
+  filter(LC_type %in% c(1:4,6,9)) |>  
+  mutate(class2 = LC_type,
+         year = as.numeric(year),
+         zone = factor(zone,levels=c(2,1,4,5,3),labels = zones$macrozona[c(2,1,4,5,3)]),
+         LC_type = factor(LC_type,levels=paleta$class,labels=paleta$Name)) -> data2 
+
+data2 |> 
+  group_by(zone) |>  
+  filter(year == 2002) |>  
+  summarize(ntot = n()) ->dataSum
+
+data2 |> 
+  group_by(zone,year,LC_type) |>  
+  summarise(n=n()) |> 
+  left_join(dataSum,by='zone') |>  
+  mutate(prop = n/ntot,sup_km2=n*0.2146587) -> data3
+
+saveRDS(data3,'data/processed_data/timeseries_landcover_zone_LCclass_2001-2021.rds')  
+
+#plot
+data3  |>  
+  ggplot(aes(year,prop)) +
+  geom_point() + 
+  geom_line()+
+  geom_smooth(method = 'lm',alpha=0.8,se = FALSE,linetype = 'dashed',col='darkblue') +
+  scale_fill_manual(name = 'Landcover class',values=colors) +
+  scale_x_continuous(breaks = seq(2000,2019,2),expand=c(0,0)) +
+  labs(y=expression(paste('Surface [',km^2,']'))) +
+  # scale_y_continuous(labels = scales::percent_format(accuracy = .1),
+  #                    expand = c(0,0)) +
+  facet_grid(LC_type~zone,scales ='free') +
+  theme_bw() +
+  theme(axis.title.x = element_blank())
+
+#linear trend
+library(tidy)
+library(broom)
+
+data3|> 
+  ungroup() |>  
+  nest_by(zone,LC_type) |>  
+  summarize(trend  = as.numeric(tidy(lm(sup_km2~year,data))[2,2])) |>  
+  pivot_wider(names_from = 'LC_type',values_from=3) |> 
+  saveRDS(.,'data/processed_data/trends_landcover_2001-2021.rds')  
