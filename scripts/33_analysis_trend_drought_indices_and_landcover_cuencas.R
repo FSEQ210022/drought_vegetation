d4da11f24443570da6ab2e220fd59115013a8152
francisco <frzambra@gmail.com>
2024-05-08 17:53:55 -0400

    update after EGU2024

--- scripts/33_analysis_trend_drought_indices_and_landcover_cuencas.R
+++ scripts/33_analysis_trend_drought_indices_and_landcover_cuencas.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(tidymodels)
+library(probably)
+
+cuenca_macro <- read_rds('data/processed_data/cuencas_macrozona.rds')
 
 data_trend_lc <- read_rds('data/processed_data/trends_landcover_cuencas_2001-2022.rds') |> 
   pivot_longer(-cuenca) |> 
@@ -23,24 +26,27 @@ data_trend_di <- read_rds('data/processed_data/df_trends_indices_cuencas.rds') |
 
 data_ana <- data_trend_di |> 
   full_join(data_trend_lc) |> 
+  full_join(cuenca_macro,by = c('cuenca' = 'COD_SUBC')) |> 
+  relocate(macrozona,.after=cuenca) |> 
+  relocate(Latitude,.after=macrozona) |> 
   drop_na()
 
 #data_model <- data_ana[,c(1:6,9:12,15:18,21:24,29,31)]
 data_model <- data_ana 
-   # mutate(
-   #   `zcNDVI-6` = cut(`zcNDVI-6`,5)
-   # )
+# mutate(
+#   `zcNDVI-6` = cut(`zcNDVI-6`,5)
+# )
 #     # landcover = cut(landcover,quantile(landcover,c(0,1/3,2/3,1)),
 #     #                 include.lowest = TRUE),
 #          class = factor(class),
 #          macro = factor(macro))
 
 data_model <- data_model |> 
-  rename('Barren_land' = `Barren land`) |> 
-  select(-cuenca)
+  rename('Barren_land' = `Barren land`)
 
 #random forest
 vars <- c("Shrubland",   "Savanna",     "Grassland",   "Barren_land", "Cropland",    "Forest")
+macrozones <- data_model |> distinct(macrozona) |> pull(macrozona)
 
 get_rf_imp <- function(x) {
   x |> 
@@ -48,130 +54,201 @@ get_rf_imp <- function(x) {
     vip::vi()
 }
 
+#probando la tendencia como categoria
+#
 # para tendencia en usos de suelo
- 
-df <- map(1:6,\(i){
-  t <- which(vars == vars[[i]])
-  df_split <- initial_split(data_model |> select(-vars[-t]),prop =.8)
-  df_train <- training(df_split)
-  df_test <- testing(df_split)
-  
-  rf_spec <- rand_forest(trees = 1000, mode = "regression") |>
-    set_args(importance = 'impurity')
-  
-  rf_wflow <- workflow(as.formula(paste0(vars[i],'~ .')), rf_spec)
-  # rf_fit <- fit(rf_wflow, df_train)
-  # 
-  #  augment(rf_fit, new_data = df_train) %>%
-  #    metrics(as.name(vars[i]),.pred)
-  # 
-  #  augment(rf_fit, new_data = df_test) %>%
-  #    metrics(vars[i],.pred)
-  #  
-  # library(vip)
-  # rf_fit |> 
-  #   extract_fit_parsnip() |> 
-  #   vip(geom = "point",num_features = 20) + 
-  #   labs(title = "Random forest variable importance") 
-  
-  ## Resampling
-  ## 
-  
-  data_folds <- vfold_cv(df_train, v = 10)
-  
-  rf_wf <- 
-    workflow() |> 
-    add_model(rf_spec) |> 
-    add_formula(as.formula(paste0(vars[i],'~ .')))
-  
-  ctrl_imp <- control_grid(extract = get_rf_imp)
-  
-  rf_fit_resample <-
-    rf_wf |> 
-    fit_resamples(data_folds,control = ctrl_imp)
 
-  #data.frame para guardar metricas
-  df_met <- collect_metrics(rf_fit_resample) |> 
-    mutate(type = vars[i]) |> 
-    select(-.estimator,-.config)
-  
-  df_var_imp <- rf_fit_resample |> 
-    select(id, .extracts) %>%
-    unnest(.extracts) %>%
-    unnest(.extracts) %>%
-    group_by(Variable) %>%
-    summarise(Mean = mean(Importance),
-              Variance = sd(Importance))
-  df_var_imp |>   
-    slice_max(Mean, n = 15) %>%
-    ggplot(aes(Mean, reorder(Variable, Mean))) +
-    geom_errorbar(aes(xmin = Mean - Variance, xmax = Mean + Variance)) +
-    geom_point(aes(Mean)) +
-    labs(x = "Variable importance", y = NULL) +
-    annotate("text",x=max(df_var_imp$Mean),y=1,label =paste0('rsq=',round(df_met$mean[2],2))) +
-    theme_bw()
-  ggsave(paste0('output/figs/fig_errorbar_resample_random_forest_trends_',vars[i],'.png'))
-  df_var_imp <- df_var_imp |> 
-    slice_max(Mean, n = 5) |> 
-    mutate(type = vars[i])
-  list(df_met,df_var_imp)
+df <- map(1:6,\(i){
+  map(macrozones,\(macro){
+    t <- which(vars == vars[[i]])
+    data_m <- data_model |> filter(macrozona == {{ macro }}) |> select(-cuenca,-macrozona,-Latitude) 
+    df_split <- initial_split(data_m |> select(-vars[-t]),prop =0.85)
+    df_train <- training(df_split)
+    df_test <- testing(df_split)
+    
+    rf_spec <- rand_forest(trees = 1000, mode = "regression") |>
+      set_args(importance = 'impurity')
+    
+    rf_wflow <- workflow(as.formula(paste0(vars[i],'~ .')), rf_spec)
+    rf_fit <- fit(rf_wflow, df_train)
+    
+    # df_test |> 
+    #   predict(rf_fit,df_test) |> 
+    #   bind_cols(trend = df_test$Cropland) |> 
+    #   ggplot(aes(.pred,trend)) + geom_point()
+    # 
+    # # 
+    #  augment(rf_fit, new_data = df_test) %>%
+    #    metrics(as.name(vars[i]),.pred)
+    # # 
+    #  augment(rf_fit, new_data = df_test) %>%
+    #    metrics(vars[i],.pred)
+    #  
+    # library(vip)
+    # rf_fit |> 
+    #   extract_fit_parsnip() |> 
+    #   vip(geom = "point",num_features = 20) + 
+    #   labs(title = "Random forest variable importance") 
+    
+    ## Resampling
+    ## 
+    
+    data_folds <- vfold_cv(df_train, v = 10)
+    
+    rf_wf <- 
+      workflow() |> 
+      add_model(rf_spec) |> 
+      add_formula(as.formula(paste0(vars[i],'~ .')))
+    
+    ctrl_imp <- control_grid(extract = get_rf_imp)
+    
+    rf_fit_resample <-
+      rf_wf |> 
+      fit_resamples(data_folds,control = ctrl_imp)
+    
+    #data.frame para guardar metricas
+    df_met <- collect_metrics(rf_fit_resample) |> 
+      mutate(type = vars[i],macrozona = {{macro}}) |> 
+      select(-.estimator,-.config)
+    
+    df_var_imp <- rf_fit_resample |> 
+      select(id, .extracts) %>%
+      unnest(.extracts) %>%
+      unnest(.extracts) %>%
+      group_by(Variable) %>%
+      summarise(Mean = mean(Importance),
+                Variance = sd(Importance))
+    df_var_imp |>   
+      slice_max(Mean, n = 15) %>%
+      ggplot(aes(Mean, reorder(Variable, Mean))) +
+      geom_errorbar(aes(xmin = Mean - Variance, xmax = Mean + Variance)) +
+      geom_point(aes(Mean)) +
+      labs(x = "Variable importance", y = NULL) +
+      annotate("text",x=max(df_var_imp$Mean),y=1,label =paste0('rsq=',round(df_met$mean[2],2))) +
+      theme_bw()
+    ggsave(paste0('output/figs/fig_errorbar_resample_random_forest_trends_',vars[i],'_',macro,'.png'),scale =1.5)
+    df_var_imp <- df_var_imp |> 
+      slice_max(Mean, n = 5) |> 
+      mutate(type = vars[i],macrozona = {{macro}}) 
+    list(df_met,df_var_imp)
+  })
 })
 
 #crear tablas
 #
 
 tabla <- map_df(1:6,\(i){
-  pluck(df,i,2)
+  map_df(1:5,\(j){
+    pluck(df,i,j,2)
+  })
 })
+
+#grafico de var importance
+#
+#paleta colores landcover persistencia
+library(broom.helpers)
+
+paleta <- read.csv('data/processed_data/paleta_colores_landcover.csv') |> 
+  dplyr::filter(Name %in% c('Forest','Shrubland','Savanna','Grassland','Cropland','Barren land'))
+
+colors <-  rgb(paleta$R,paleta$G,paleta$B,maxColorValue = 255)
+#attr(colors,'names') <- paleta$class
+paleta$Name[6] <- 'Barren_land'
+attr(colors,'names') <- paleta$Name
+
+tabla |>
+  mutate(type = factor(type,levels=paleta$Name),
+         macrozona = factor(macrozona,levels=c("norte grande", "norte chico",
+                                               'zona central','zona sur','zona austral'),
+                            labels = c('Norte Grande','Norte Chico','Centro','Sur','Austral')),
+         Variable = .clean_backticks(Variable),
+         Mean = case_when(macrozona == "Norte Grande" & (type == "Forest" | type == "Cropland" | type == "Savanna") ~ NA,
+                          macrozona == "Norte Chico" & type == "Forest" ~ NA,
+                          macrozona == "Sur" & type == "Shrubland" ~ NA,
+                          macrozona == "Austral" & type == "Cropland" ~ NA,
+                          .default = Mean)
+  ) |> 
+  group_by(type,macrozona) |> 
+  mutate(rel_imp = Mean*10e2) |> 
+  slice_max(rel_imp, n= 1) |> 
+  ggplot(aes(Variable,rel_imp)) +
+  geom_col(aes(fill = type),position = 'dodge') +
+  scale_y_continuous(expand = c(0,0)) +
+  scale_fill_manual(name = 'Landcover class',
+                    values=colors) +
+  labs(y = 'Relative variable importance') +
+  coord_flip() +
+  facet_grid(macrozona~.) +
+  theme_bw() +
+  theme(strip.background = element_rect('white'),
+        legend.key.size = unit(0.2, "cm"),
+        legend.title = element_text(size=10),
+        legend.text = element_text(size=8),
+        panel.grid = element_blank(),
+        axis.title.y = element_blank(),
+        legend.position = c(.9,.85))
+ggsave('output/figs/bars_relative_importance_RF.png',height = 10,width=7,scale = 1.2)
+
 
 tabla2 <- map_df(1:6,\(i){
-  pluck(df,i,1)
+  map_df(1:5,\(j){
+    pluck(df,i,j,1)
+  })
 })
 
-tabla2 <- tabla2 |> 
+r2s <- tabla2 |> 
   filter(.metric == 'rsq') |> 
-  select(-std_err,-n) |> 
+  select(-std_err,-n) |>
+  # group_by(type) |> 
+  # summarize(mean = mean(mean)) |> 
   pivot_wider(names_from = type,values_from = mean) |> 
-  select(.metric,Forest,Cropland,Grassland, Savanna,Shrubland,Barren_land )
+  select(Forest,Cropland,Grassland, Savanna,Shrubland,Barren_land )
 
-head <- paste0(names(tabla2[2:7]),'\n (rsq=',round(tabla2[1,2:7],2),')')
-head <- c('Position',head)
+head <- c('Macrozone',paste0(names(r2s),'\n (R<sup>2=',round(r2s[1,],2),')'))
 
 library(gt)
-library(broom.helpers)
-tabla |> 
-  mutate(Variable = .clean_backticks(Variable))  |>
+tabla_gt <- tabla |> 
+  mutate(macrozona = factor(macrozona),
+         macrozona = fct_relevel(macrozona,c('norte grande','norte chico','zona central','zona sur','zona austral')),
+         macrozona =factor(macrozona,labels = c('Norte Grande','Norte Chico','Centro','Sur','Austral'))) |> 
+  group_by(type,macrozona) |> 
+  slice_max(Mean,n =1) |> 
   select(-Mean,-Variance) |> 
-  mutate(pos = rep(1:5,6)) |> 
+  mutate(Variable = .clean_backticks(Variable)) |>
   pivot_wider(names_from = type,values_from = Variable) |> 
-  select(pos,Forest,Cropland,Grassland, Savanna,Shrubland,Barren_land ) |> 
-  set_names(head) |> 
-  gt() |> 
-  cols_align(align = 'center') |> 
-  gt::gtsave('output/figs/table_importance_trends_landcover_vs_drought.png')
+  select(Forest,Cropland,Grassland, Savanna,Shrubland,Barren_land ) |> 
+  bind_cols(set_names(r2s,paste0(names(r2s),'r2')))
 
-# para zcNDVI
 
-df_split <- initial_split(data_model,prop =.8)
-df_train <- training(df_split)
-df_test <- testing(df_split)
+tabla_gt |> 
+  rename(Macrozone = macrozona,
+         `Barren Land` = Barren_land) |> 
+  ungroup() |> 
+  gt() %>% 
+  #opt_stylize(style = 6, color = 'gray')
+  data_color(
+    columns = 8:13,
+    target_columns =2:7,
+    palette = rev(viridis::inferno(20)),
+    na_color = 'white',
+    domain = c(0,.51),
+    alpha = .8
+  ) |> 
+  cols_hide(columns = 8:13) |> 
+  tab_footnote(
+    footnote = html(local_image('output/figs/leyenda_tabla_r2_random_forest.png',height = 250))) |> 
+  gt::gtsave('output/figs/table_r2_most_important_variables.png')  
 
-rf_spec <- rand_forest(trees = 1000, mode = "regression") |>
-  set_args(importance = 'impurity')
-# lasso_spec <- linear_reg(penalty = 0.1, mixture = 1) %>%
-#   set_engine("glmnet")
+#crear legenda para incluir en la tabla
+plot <- r2s |> mutate(id=1:5) |> 
+  pivot_longer(-id) |> 
+  ggplot(aes(name,value,color=value)) + 
+  geom_point() + 
+  scale_color_viridis_c(option = 'inferno',
+                        name = 'r-squared',
+                        direction = -1,
+                        alpha = .8) + theme(legend.position = 'bottom')
 
-rf_wflow <- workflow(as.formula(`zcNDVI-6`~.), rf_spec)
-rf_fit <- fit(rf_wflow, df_train)
-
- augment(rf_fit, new_data = df_train) %>%
-   metrics(as.name(vars[i]),.pred)
-
- augment(rf_fit, new_data = df_test) %>%
-   metrics(vars[i],.pred)
-
- library(vip)
- rf_fit |>
-   extract_fit_parsnip() |>
-   vip(geom = "point",num_features = 20) +
-   labs(title = "Random forest variable importance")
+ggpubr::get_legend(plot) |> 
+  ggpubr::as_ggplot() |> 
+  ggsave(filename = 'output/figs/leyenda_tabla_r2_random_forest.png')
