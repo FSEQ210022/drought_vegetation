3b2727e8bbfd79fdf089368ef66a50606f3e7c3e
frzambra <frzambra@gmail.com>
2024-12-19 15:52:59 -0300

    actualizar repo

---
+++ scripts/47_calcular_tendencia_area_quemada.R
@@ -0,0 +1,44 @@
+library(terra)
+library(fs)
+library(rnaturalearth)
+library(sf)
+library(tidyverse)
+library(trend)
+
+# datos almacenados en el computador de escritorio
+dir <- '/media/francisco/data_raw/Otros/incendios_monthly'
+files <- dir_ls(dir,regexp = 'nc$')
+
+chl <- ne_countries(country = 'chile',scale = 'medium',returnclass = 'sf')
+chl_cont <- st_as_sf(chl) |> 
+  st_cast('POLYGON') |> 
+  slice(-(1:2)) 
+
+
+arque_anual <- seq_along(files) |> 
+  map(\(i){
+    arque <- rast(dir_ls(dir)[i]) 
+
+    arque['burned_area'] |> 
+      crop(vect(chl_cont)) |>
+      mask(vect(chl_cont)) |> 
+      sum(na.rm = TRUE) 
+  })
+
+arque_anual <- rast(arque_anual)
+names(arque_anual) <- 2002:2022
+plot(arque_anual)
+
+
+#Calcular la tendencia de el área quemada entre 2002 y 2022
+
+source('R/trend_func.R')
+
+library(modifiedmk)
+nc <- parallel::detectCores()
+cl <- snow::makeCluster(nc-2,"SOCK")
+snow::clusterExport(cl, list = c('trend_func2','mmky'))
+trend <- app(arque_anual,trend_func2,cores = cl)
+snow::stopCluster(cl)
+
+writeRaster(trend,'~/Documentos/trend_area_quemada_2002-2022.tif')
---
+++ scripts/48_calcular_tendencia_luces_nocturnas.R
@@ -0,0 +1,37 @@
+library(terra)
+library(fs)
+library(tidyverse)
+library(rnaturalearth)
+library(sf)
+
+#descarga los límites de chile
+chl <- ne_countries(country = 'chile',scale = 'medium',returnclass = 'sf')
+
+#extrae las islas para dejar las geometrías de Chile continetal
+chl_cont <- st_as_sf(chl) |> 
+  st_cast('POLYGON') |> 
+  slice(-(1:2)) 
+
+dir <- '/media/francisco/data_raw/Otros/VNP46A4'
+files <- dir_ls(dir,regexp = 'tif$')
+
+out <- files |> 
+  map(\(file){
+    rast(file) |> 
+      crop(vect(chl_cont)) |> 
+      mask(vect(chl_cont))
+  })
+    
+nbm_nl <- rast(out)
+
+plot(nbm_nl)
+
+#Calcular la tendencia de las luces nocturnas entre 2012 a 2023
+
+source('R/trend_func.R')
+
+library(modifiedmk)
+
+trend <- app(nbm_nl,trend_func2)
+
+writeRaster(trend,'~/Documentos/trend_nasa_marble_luces_nocturnas_2012-2023.tif')
